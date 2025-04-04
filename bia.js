ae60cdc718d9537c824d136dd8884557d1b4d933
jpschinato <jpsilvaschinato@gmail.com>
2025-04-03 21:03:33 -0300

    Corrigindo erro de deploy

--- bia.js
+++ bia.js
@@ -1,36 +1,42 @@
 let slideIndex = 0;
-function showSlides() {
-    let slides = document.getElementsByClassName("slides");
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-        slides[i].classList.remove("active"); // Remove classe de borda ativa
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1; }
-    slides[slideIndex - 1].style.display = "block";
-    slides[slideIndex - 1].classList.add("active"); // Adiciona classe de borda ativa
-    setTimeout(showSlides, 3000);
-}
-showSlides();
+        function showSlides() {
+            let slides = document.getElementsByClassName("slides");
+            for (let i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            slideIndex++;
+            if (slideIndex > slides.length) { slideIndex = 1; }
+              slides[slideIndex - 1].style.display = "block";
+            setTimeout(showSlides, 3000);
+        }
+        showSlides();
 
-function updateCounter() {
-    let startDate = new Date("2024-09-08T00:00:00-03:00");
-    let today = new Date(new Date().toLocaleString("en-US", {timeZone: "America/Sao_Paulo"}));
-    let diffTime = today - startDate;
-    let diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
-    document.getElementById("contador").innerHTML = `Estamos juntos há ${diffDays} dias! 🤍💍`;
-}
-function updateCounterConheci() {
-    let startDateConheci = new Date("2024-04-06T00:00:00-03:00");
-    let today = new Date(new Date().toLocaleString("en-US", {timeZone: "America/Sao_Paulo"}));
-    let diffTimeConheci = today - startDateConheci;
-    let diffDaysConheci = Math.floor(diffTimeConheci / (1000 * 60 * 60 * 24));
-    document.getElementById("contadorConheci").innerHTML = `Nos conhecemos há ${diffDaysConheci} dias! 🤍`;
-}
-updateCounter();
-updateCounterConheci();
+        function updateCounter() {
+            let startDate = new Date("2024-09-08T00:00:00-03:00");
+            let today = new Date(new Date().toLocaleString("en-US", {timeZone: "America/Sao_Paulo"}));
+            let diffTime = today - startDate;
+            let diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+            let diffHours = Math.floor((diffTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            let diffMinutes = Math.floor((diffTime % (1000 * 60 * 60)) / (1000 * 60));
+            document.getElementById("contador").innerHTML = `Estamos juntos há ${diffDays} dias, ${diffHours} horas e ${diffMinutes} minutos! 🤍💍`;
+        }
+        function updateCounterConheci() {
+            let startDateConheci = new Date("2024-04-06T00:00:00-03:00");
+            let today = new Date(new Date().toLocaleString("en-US", {timeZone: "America/Sao_Paulo"}));
+            let diffTimeConheci = today - startDateConheci;
+            let diffDaysConheci = Math.floor(diffTimeConheci / (1000 * 60 * 60 * 24));
+            let diffHoursConheci = Math.floor((diffTimeConheci % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            let diffMinutesConheci = Math.floor((diffTimeConheci % (1000 * 60 * 60)) / (1000 * 60));
+            document.getElementById("contadorConheci").innerHTML = `Nos conhecemos há ${diffDaysConheci} dias, ${diffHoursConheci} horas e ${diffMinutesConheci} minutos! 🤍`;
+        }
+        updateCounter();
+        updateCounterConheci();
 
-setInterval(() => {
-    updateCounter();
-    updateCounterConheci();
-}, 60000);
+        function atualizarDiariamente() {
+            setInterval(() => {
+                updateCounter();
+                updateCounterConheci();
+            }, 60000);
+        }
+
+        atualizarDiariamente();
